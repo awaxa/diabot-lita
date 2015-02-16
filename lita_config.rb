727afafc37fe64a069cd8f5fee157b5810cc67e7
Greg Kitson <greg.kitson@gmail.com>
2015-02-16 10:07:48 -0800

    move fast and break stuff

--- lita_config.rb
+++ lita_config.rb
@@ -1,45 +1,37 @@
+begin
+  require 'pry'
+rescue LoadError
+end
+
 Lita.configure do |config|
 
   options = {
-    :name => ENV["LITA_NAME"] || "diabot",
+    :name     => ENV["LITA_NAME"] || "diabot",
     :channels => ENV["LITA_CHANNELS"] || "##diabot",
+    :admins   => ENV["LITA_ADMINS"] || "",
+    :password => ENV["LITA_NICKSERV_PASSWORD"] || "",
   }
 
-  # The name your robot will use.
   config.robot.name = options[:name]
 
-  # The locale code for the language to use.
   config.robot.locale = :en
 
-  # The severity of messages to log. Options are:
-  # :debug, :info, :warn, :error, :fatal
-  # Messages at the selected level and above will be logged.
   config.robot.log_level = :debug
 
-  # An array of user IDs that are considered administrators. These users
-  # the ability to add and remove other users from authorization groups.
-  # What is considered a user ID will change depending on which adapter you use.
-  # config.robot.admins = ["1", "2"]
+  config.robot.admins = options[:admins].split(',') unless options[:admins].empty?
 
-  # The adapter you want to connect with. Make sure you've added the
-  # appropriate gem to the Gemfile.
   config.robot.adapter = :irc
   config.adapters.irc.server = "irc.freenode.net"
-  config.adapters.irc.channels = options[:channels].split(',')
+  config.adapters.irc.channels = options[:channels].split(",")
   config.adapters.irc.log_level = :debug
+  config.adapters.irc.user = options[:name]
+  config.adapters.irc.password = options[:password] unless options[:password].empty?
+  config.adapters.irc.realname = options[:name]
   config.adapters.irc.cinch = lambda do |cinch_config|
     cinch_config.max_reconnect_delay = 123
     cinch_config.port = "6697"
     cinch_config.ssl.use = true
   end
-
-  ## Example: Set options for the chosen adapter.
-  # config.adapter.username = "myname"
-  # config.adapter.password = "secret"
-
-  ## Example: Set options for the Redis connection.
-  # config.redis.host = "127.0.0.1"
-  # config.redis.port = 1234
 
   platform = :heroku if ENV["DYNO"]
   platform = :boxen if ENV["BOXEN_SETUP_VERSION"]
@@ -53,7 +45,4 @@ Lita.configure do |config|
     config.redis[:url] = ENV["LITA_REDIS_URL"] || "redis://127.0.0.1:6379/"
   end
 
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
 end
