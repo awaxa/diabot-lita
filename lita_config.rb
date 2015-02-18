c19b4d7778518db2566ad3b1b5b5c85a0ebc7266
Greg Kitson <greg.kitson@gmail.com>
2015-02-17 20:02:42 -0800

    add lita-reddit

--- lita_config.rb
+++ lita_config.rb
@@ -6,10 +6,12 @@ end
 Lita.configure do |config|
 
   options = {
-    :name     => ENV["LITA_NAME"] || "diabot",
-    :channels => ENV["LITA_CHANNELS"] || "##diabot",
-    :admins   => ENV["LITA_ADMINS"] || "",
-    :password => ENV["LITA_NICKSERV_PASSWORD"] || "",
+    :name                 => ENV["LITA_NAME"] || "diabot",
+    :channels             => ENV["LITA_CHANNELS"] || "##diabot",
+    :admins               => ENV["LITA_ADMINS"] || "",
+    :nickserv_password    => ENV["LITA_NICKSERV_PASSWORD"] || "",
+    :reddit_client_id     => ENV["LITA_REDDIT_CLIENT_ID"] || "",
+    :reddit_client_secret => ENV["LITA_REDDIT_CLIENT_SECRET"] || "",
   }
 
   config.robot.name = options[:name]
@@ -25,7 +27,7 @@ Lita.configure do |config|
   config.adapters.irc.channels = options[:channels].split(",")
   config.adapters.irc.log_level = :debug
   config.adapters.irc.user = options[:name]
-  config.adapters.irc.password = options[:password] unless options[:password].empty?
+  config.adapters.irc.password = options[:nickserv_password] unless options[:nickserv_password].empty?
   config.adapters.irc.realname = options[:name]
   config.adapters.irc.cinch = lambda do |cinch_config|
     cinch_config.max_reconnect_delay = 123
@@ -46,5 +48,11 @@ Lita.configure do |config|
   end
 
   config.handlers.keepalive.url = "http://#{options[:name]}.herokuapp.com"
+
+  config.handlers.reddit.reddits = [
+    { subreddit: "diabetes", channel: "##diabot" },
+  ]
+  config.handlers.reddit.client_id = options[:reddit_client_id]
+  config.handlers.reddit.client_secret = options[:reddit_client_secret]
 
 end
