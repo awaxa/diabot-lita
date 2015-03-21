ab74ef1f61653d18008a922ffea427fbb7e279b4
Greg Kitson <greg.kitson@gmail.com>
2015-03-21 10:01:32 -0700

    rearrange lita_config.rb

--- lita_config.rb
+++ lita_config.rb
@@ -5,57 +5,48 @@ end
 
 Lita.configure do |config|
 
-  options = {
-    :name                 => ENV["LITA_NAME"] || "diabot",
-    :channels             => ENV["LITA_CHANNELS"] || "##diabot",
-    :admins               => ENV["LITA_ADMINS"] || "",
-    :nickserv_password    => ENV["LITA_NICKSERV_PASSWORD"] || "",
-    :reddit_client_id     => ENV["LITA_REDDIT_CLIENT_ID"] || "",
-    :reddit_client_secret => ENV["LITA_REDDIT_CLIENT_SECRET"] || "",
-    :log_level            => ENV["LITA_LOG_LEVEL"] || "info",
-    :irc_log_level        => ENV["LITA_IRC_LOG_LEVEL"] || "info",
-  }
+  options = {}
+  options[:name] = "diabutt"
+  options[:channels] = [ "##diabot" ]
+
+  platform = :boxen  if ENV["BOXEN_SETUP_VERSION"]
+  platform = :heroku if ENV["DYNO"]
+  case platform
+  when :heroku
+    options[:name]     = "diabot"
+    options[:channels] = [ "#reddit-diabetes" , "#reddit-diabetes-ops" ]
+    config.http.port   = ENV["PORT"]
+    config.redis[:url] = ENV["REDISCLOUD_URL"]
+  when :boxen
+    config.redis[:url] = ENV["BOXEN_REDIS_URL"]
+  else
+    config.redis[:url] = ENV["LITA_REDIS_URL"] || "redis://127.0.0.1:6379/"
+  end
+
+  config.handlers.reddit.client_id     = ENV["LITA_REDDIT_CLIENT_ID"] || ""
+  config.handlers.reddit.client_secret = ENV["LITA_REDDIT_CLIENT_SECRET"] || ""
+  config.handlers.reddit.reddits = [
+    { subreddit: "diabetes", channel: "#reddit-diabetes" },
+  ]
+
+  config.handlers.keepalive.url = "http://#{options[:name]}.herokuapp.com"
 
   config.robot.name = options[:name]
-
   config.robot.locale = :en
-
-  config.robot.log_level = options[:log_level].to_sym
-
-  config.robot.admins = options[:admins].split(',') unless options[:admins].empty?
-
+  config.robot.log_level = :debug
   config.robot.adapter = :irc
   config.adapters.irc.server = "irc.freenode.net"
-  config.adapters.irc.channels = options[:channels].split(",")
-  config.adapters.irc.log_level = options[:irc_log_level].to_sym
-  unless options[:nickserv_password].empty?
+  config.adapters.irc.channels = options[:channels]
+  config.adapters.irc.log_level = :info
+  if ENV["LITA_NICKSERV_PASSWORD"]
     config.adapters.irc.user = "diabot"
-    config.adapters.irc.password = options[:nickserv_password]
+    config.adapters.irc.password = ENV["LITA_NICKSERV_PASSWORD"]
   end
   config.adapters.irc.realname = options[:name]
   config.adapters.irc.cinch = lambda do |cinch_config|
     cinch_config.max_reconnect_delay = 123
     cinch_config.port = "6697"
     cinch_config.ssl.use = true
-  end
-
-  config.handlers.keepalive.url = "http://#{options[:name]}.herokuapp.com"
-
-  config.handlers.reddit.client_id = options[:reddit_client_id]
-  config.handlers.reddit.client_secret = options[:reddit_client_secret]
-  config.handlers.reddit.reddits = []
-
-  platform = :boxen  if ENV["BOXEN_SETUP_VERSION"]
-  platform = :heroku if ENV["DYNO"]
-  case platform
-  when :heroku
-    config.http.port = ENV["PORT"]
-    config.redis[:url] = ENV["REDISCLOUD_URL"]
-    config.handlers.reddit.reddits << { subreddit: "diabetes", channel: "#reddit-diabetes" }
-  when :boxen
-    config.redis[:url] = ENV["BOXEN_REDIS_URL"]
-  else
-    config.redis[:url] = ENV["LITA_REDIS_URL"] || "redis://127.0.0.1:6379/"
   end
 
 end
