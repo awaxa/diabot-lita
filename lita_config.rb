6c3807cad52342793d35516e3b5b4e8eaa74ec38
Phil Porada <philporada@gmail.com>
2017-06-26 11:35:40 -0400

    Ran rubocop

--- lita_config.rb
+++ lita_config.rb
@@ -4,25 +4,24 @@ rescue LoadError
 end
 
 Lita.configure do |config|
-
   options = {}
-  options[:name] = "diabot"
-  options[:channels] = [ "#reddit-diabetes-ops-debug" ]
+  options[:name] = 'diabot'
+  options[:channels] = ['#reddit-diabetes-ops-debug']
 
-  platform = :heroku if ENV["DYNO"]
+  platform = :heroku if ENV['DYNO']
   case platform
   when :heroku
-    options[:channels] << [ "#reddit-diabetes" , "#reddit-diabetes-ops" ]
-    config.http.port   = ENV["PORT"]
-    config.redis[:url] = ENV["REDISCLOUD_URL"]
+    options[:channels] << ['#reddit-diabetes', '#reddit-diabetes-ops']
+    config.http.port   = ENV['PORT']
+    config.redis[:url] = ENV['REDISCLOUD_URL']
   else
-    config.redis[:url] = ENV["LITA_REDIS_URL"] || "redis://127.0.0.1:6379/"
+    config.redis[:url] = ENV['LITA_REDIS_URL'] || 'redis://127.0.0.1:6379/'
   end
 
-  config.handlers.reddit.client_id     = ENV["LITA_REDDIT_CLIENT_ID"] || ""
-  config.handlers.reddit.client_secret = ENV["LITA_REDDIT_CLIENT_SECRET"] || ""
+  config.handlers.reddit.client_id     = ENV['LITA_REDDIT_CLIENT_ID'] || ''
+  config.handlers.reddit.client_secret = ENV['LITA_REDDIT_CLIENT_SECRET'] || ''
   config.handlers.reddit.reddits = [
-    { subreddit: "diabetes", channel: "#reddit-diabetes-ops-debug" },
+    { subreddit: 'diabetes', channel: '#reddit-diabetes-ops-debug' }
   ]
 
   config.handlers.keepalive.url = "http://#{options[:name]}.herokuapp.com"
@@ -31,19 +30,18 @@ Lita.configure do |config|
   config.robot.locale = :en
   config.robot.log_level = :debug
   config.robot.adapter = :irc
-  config.adapters.irc.server = "irc.freenode.net"
+  config.adapters.irc.server = 'irc.freenode.net'
   config.adapters.irc.channels = options[:channels].flatten
   config.adapters.irc.log_level = :debug
-  config.adapters.irc.user = "diabot"
+  config.adapters.irc.user = 'diabot'
   config.adapters.irc.realname = options[:name]
   config.adapters.irc.cinch = lambda do |cinch_config|
-    cinch_config.sasl.username = "diabot"
-    if ENV["LITA_NICKSERV_PASSWORD"]
-      cinch_config.sasl.password = ENV["LITA_NICKSERV_PASSWORD"]
+    cinch_config.sasl.username = 'diabot'
+    if ENV['LITA_NICKSERV_PASSWORD']
+      cinch_config.sasl.password = ENV['LITA_NICKSERV_PASSWORD']
     end
     cinch_config.max_reconnect_delay = 123
-    cinch_config.port = "6697"
+    cinch_config.port = '6697'
     cinch_config.ssl.use = true
   end
-
 end
